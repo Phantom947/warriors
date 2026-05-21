1d2b2a3c6dec4847c11f83c5d6044d11fabfb458
Phantom947 <acm9@smmk12.org>
2026-05-20 20:10:09 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,134 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "P2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . 4 . . 
+. . . . . . . . . . . . 4 3 . . 
+. . . . . . . . . . . . 4 4 4 . 
+. . . . . . . . . . . . e e 7 4 
+. . . 4 4 e 4 e 4 e 4 4 4 4 4 4 
+. . 4 4 4 4 4 e 4 4 4 e e e . . 
+. . 4 . 4 4 4 4 4 4 4 4 e e . . 
+. . 4 . 4 4 4 4 4 4 4 4 e . . . 
+. 4 4 . 4 . 4 . . . 4 . 4 . . . 
+e e . . e . e . . . e . e . . . 
+. . . . e . e . . . e . e . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "demon dog":
+            case "anim1":return [img`
+bbbb........bbbb.................
+c99bb......bb99b.................
+c999bb....bb999c.................
+c9b99bccccb99b9c.................
+c9bb99bccb99bb9c.................
+c93b99999999b39c.................
+c93399999999339c.................
+c99399999999399c.................
+c99999991199999c.................
+c999ff91119ff99c........bbbbbb...
+c999ff91111ff99c.......c999999bb.
+c99991111111999c......c99999999b.
+c9991111fff1199c.....c9991119999b
+c999c11fff1199bc.....c9911111999b
+c999cc111111c9bc.....c911dd11199b
+c99999bb33cc99bcc....cbddbbd1199c
+c999999b33c99999bbccccbbdbbb1199c
+c9999999bb9999999999999999bb1999c
+c999911119999999999999999999b999c
+c999111111999999999999999999999c.
+c99911111119999999999999999999cc.
+c99111111119999999999999999999c..
+c99111111111999999999999999999c..
+cb9111111111999999999999999999c..
+.f9111111111999999999999999999c..
+.ff111111111999999999999999999c..
+..fb11111111999999999999999999c..
+...fb1111119999999111111999999c..
+...fbbb11119999991111111199999c..
+....fbbfffb9999ccccccccccb9999c..
+....fbbf..f999c.....fbbf.c9999c..
+....fbbf..f999c.....fbbf.cc9999c.
+....fbbf..f99c.......fbf..cc999c.
+....fbbf..f99c.......fbbf..cc99c.
+....fbbf..f99c.......fbbf...c99c.
+....fbbf..f99c......fbbbf...c99c.
+...fbbbf..f99c......ffff....cb9c.
+...fbbf..f999c.............c999c.
+...ffff..f99cc.............c999c.
+.........fffc..............cccc..
+`, img`
+bbbb........bbbb.................
+cffbb......bbffb.................
+cfffbb....bbfffc.................
+cfbffbccccbffbfc.................
+cfbbffbccbffbbfc.................
+cf3bffffffffb3fc.................
+cf33ffffffff33fc.................
+cff3ffffffff3ffc.................
+cff2222f112222fc.................
+cfff222111222ffc........bbbbbb...
+cffff2211122fffc.......cffffffbb.
+cffff1111111fffc......cffffffffb.
+cfff1111fff11ffc.....cfff111ffffb
+cfffc11fff11ffbc.....cff11111fffb
+cfffcc111111cfbc.....cf11dd111ffb
+cfffffbb33ccffbcc....cbddbbd11ffc
+cffffffb33cfffffbbccccbbdbbb11ffc
+cfffffffbbffffffffffffffffbb1fffc
+cffff1111fffffffffffffffffffbfffc
+cfff111111fffffffffffffffffffffc.
+cfff1111111fffffffffffffffffffcc.
+cff11111111fffffffffffffffffffc..
+cff111111111ffffffffffffffffffc..
+cbf111111111ffffffffffffffffffc..
+.ff111111111ffffffffffffffffffc..
+.ff111111111ffffffffffffffffffc..
+..fb11111111ffffffffffffffffffc..
+...fb111111fffffff111111ffffffc..
+...fbbb1111ffffff11111111fffffc..
+....fbbfffbffffccccccccccbffffc..
+....fbbf..ffffc.....fbbf.cffffc..
+....fbbf..ffffc.....fbbf.ccffffc.
+....fbbf..fffc.......fbf..ccfffc.
+....fbbf..fffc.......fbbf..ccffc.
+....fbbf..fffc.......fbbf...cffc.
+....fbbf..fffc......fbbbf...cffc.
+...fbbbf..fffc......ffff....cbfc.
+...fbbf..ffffc.............cfffc.
+...ffff..fffcc.............cfffc.
+.........fffc..............cccc..
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
